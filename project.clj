392b6a1060e85f91211a96789246c4a856f3341f
mkhodeir <mostafa.khodeir@zalando.de>
2018-10-09 11:53:41 +0200

    Adding Landing Page

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.4.474"]  
+                 [com.taoensso/timbre "4.10.0"]
+                 [com.grammarly/omniconf "0.3.2"]
                  [compojure "1.6.1"]
+                 [hiccup "1.0.5"]
+                 [ring "1.7.0"]
+                 [ring-server "0.5.0"]
                  [ring/ring-defaults "0.3.2"]]
   :plugins [[lein-ring "0.12.4"]]
   :ring {:handler chroma-bot.handler/app}
--- src/chroma_bot/handler.clj
+++ src/chroma_bot/handler.clj
@@ -1,10 +1,11 @@
 (ns chroma-bot.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [chroma-bot.static :as static ] ))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] static/home)
   (route/not-found "Not Found"))
 
 (def app
